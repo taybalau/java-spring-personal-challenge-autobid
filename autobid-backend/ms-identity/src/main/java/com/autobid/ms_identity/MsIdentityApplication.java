f451dffd10590486e37404256f66482792be28c1
Taynara Vitorino <taynaravitorino@gmail.com>
2026-02-18 01:54:15 -0300

    chore(infra): initial setup

---
+++ autobid-backend/ms-identity/src/main/java/com/autobid/ms_identity/MsIdentityApplication.java
@@ -0,0 +1,13 @@
+package com.autobid.ms_identity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsIdentityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsIdentityApplication.class, args);
+	}
+
+}
---
+++ autobid-backend/ms-identity/src/test/java/com/autobid/ms_identity/MsIdentityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autobid.ms_identity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsIdentityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
